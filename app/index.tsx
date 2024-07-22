563e7ab0f9220601f7cc35a03dab09e157cb72fa
SiquaraDev <matheus.siquara99@gmail.com>
2024-07-22 14:38:44 -0300

    update index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,28 @@
 import Page from "@/components/template/Page";
-import { Text } from "react-native";
+import { SafeAreaView } from "react-native";
 import Logo from "./../components/template/Logo";
+import Question from "@/components/quiz/Question";
+import useQuiz from "@/data/hooks/useQuiz";
+import Points from "@/components/quiz/Points";
 
 export default function HomeScreen() {
+    const { question, concluded, points, totalQuestions, reset, answer } =
+        useQuiz();
+
     return (
         <Page>
-            <Logo />
+            <SafeAreaView style={{ gap: 40 }}>
+                <Logo />
+                {concluded ? (
+                    <Points
+                        points={points}
+                        totalQuestions={totalQuestions}
+                        reset={reset}
+                    />
+                ) : (
+                    <Question question={question} selectOption={answer} />
+                )}
+            </SafeAreaView>
         </Page>
     );
 }
